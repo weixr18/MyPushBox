91c45093e1a66c9d0480074ae696e06817695436
weixr18 <weixr18@mails.tsinghua.edu.cn>
2020-10-04 12:14:20 +0800

    reconstitution complete

---
+++ src/BoxGame.cs
@@ -0,0 +1,327 @@
+﻿using System;
+
+namespace MyPushBox
+{
+
+    public enum GridType
+    {
+        Ground = 1,
+        Target = 2,
+        Box = 3,
+        RedBox = 4,
+        Player = 5,
+        TarPlayer = 6,
+        Brick = 7,
+        OutSide = 8,
+
+        // RedBox 只能和 Target 互相转化！！！
+    }
+
+    public enum PlayerOperation
+    {
+        MoveUp,
+        MoveRight,
+        MoveDown,
+        MoveLeft,
+    }
+
+    public class Player
+    {
+        public int x;
+        public int y;
+
+        public Player(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+
+    public struct BoardInfo
+    {
+        public int rowNum { get; }
+        public int columnNum { get; }
+        public GridType[,] d;
+        public Player p;
+
+        public BoardInfo(int r, int c, Player _p)
+        {
+            rowNum = r;
+            columnNum = c;
+            d = new GridType[r, c];
+            p = _p;
+        }
+    }
+
+
+
+    public class GameEngine {
+
+        private BoardInfo Board;
+
+        public int rowNum 
+        { 
+            get { return Board.rowNum; }
+        }
+
+        public int columnNum 
+        {
+            get { return Board.columnNum; }
+        }
+
+        public GridType[,] d 
+        {
+            get { return Board.d; }
+        }
+
+        public Player p
+        {
+            get { return Board.p; }
+            set { Board.p = (value); }
+        }
+
+        public GameEngine(BoardInfo b) {
+            Board = b;
+        }
+
+        public bool PlayerOperate(PlayerOperation o)
+        {
+
+            /**
+             * Get variables.
+             */
+
+            int bg_y = -1;
+            int bg_x = -1;
+            int nxt_y = -1;
+            int nxt_x = -1;
+            bool hasNextGrid = false;
+
+            switch (o)
+            {
+                case PlayerOperation.MoveUp:
+                {
+                    if (Board.p.y == 0)
+                    {
+                        return false;
+                    }
+                    bg_y = Board.p.y - 1;
+                    bg_x = Board.p.x;
+                    nxt_y = Board.p.y - 2;
+                    nxt_x = Board.p.x;
+                    hasNextGrid = (Board.p.y > 1);
+
+                    break;
+                }
+                case PlayerOperation.MoveDown:
+                {
+                    if (Board.p.y == Board.rowNum - 1)
+                    {
+                        return false;
+                    }
+
+                    bg_y = Board.p.y + 1;
+                    bg_x = Board.p.x;
+                    nxt_y = Board.p.y + 2;
+                    nxt_x = Board.p.x;
+                    hasNextGrid = (Board.p.y < Board.rowNum - 2);
+
+                    break;
+                }
+                case PlayerOperation.MoveLeft:
+                {
+                    if (Board.p.x == 0)
+                    {
+                        return false;
+                    }
+
+                    bg_y = Board.p.y;
+                    bg_x = Board.p.x - 1;
+                    nxt_y = Board.p.y;
+                    nxt_x = Board.p.x - 2;
+                    hasNextGrid = (Board.p.x > 1);
+
+                    break;
+                }
+                case PlayerOperation.MoveRight:
+                {
+                    if (Board.p.x == Board.columnNum - 1)
+                    {
+                        return false;
+                    }
+
+                    bg_y = Board.p.y;
+                    bg_x = Board.p.x + 1;
+                    nxt_y = Board.p.y;
+                    nxt_x = Board.p.x + 2;
+                    hasNextGrid = (Board.p.x < Board.columnNum - 2);
+
+                    break;
+                }
+            }
+
+            /**
+             * Logic Judgement.
+             */
+
+            if (Board.d[bg_y, bg_x] == GridType.Brick)
+            {
+                return false;
+            }
+            // move to ground
+            else if (Board.d[bg_y, bg_x] == GridType.Ground)
+            {
+                Board.d[bg_y, bg_x] = GridType.Player;
+
+                if (Board.d[Board.p.y, Board.p.x] == GridType.TarPlayer)
+                {
+                    Board.d[Board.p.y, Board.p.x] = GridType.Target;
+                }
+                else
+                {
+                    Board.d[Board.p.y, Board.p.x] = GridType.Ground;
+                }
+            }
+            // move to target
+            else if (Board.d[bg_y, bg_x] == GridType.Target)
+            {
+                Board.d[bg_y, bg_x] = GridType.TarPlayer;
+                if (Board.d[Board.p.y, Board.p.x] == GridType.TarPlayer)
+                {
+                    Board.d[Board.p.y, Board.p.x] = GridType.Target;
+                }
+                else
+                {
+                    Board.d[Board.p.y, Board.p.x] = GridType.Ground;
+                }
+            }
+            // push box
+            else if (Board.d[bg_y, bg_x] == GridType.Box)
+            {
+                if (!hasNextGrid)
+                {
+                    return false;
+                }
+
+                if (Board.d[nxt_y, nxt_x] == GridType.Ground
+                    || Board.d[nxt_y, nxt_x] == GridType.Target)
+                {
+                    // self
+                    if (Board.d[Board.p.y, Board.p.x] == GridType.TarPlayer)
+                    {
+                        Board.d[Board.p.y, Board.p.x] = GridType.Target;
+                    }
+                    else
+                    {
+                        Board.d[Board.p.y, Board.p.x] = GridType.Ground;
+                    }
+
+                    // bg
+                    Board.d[bg_y, bg_x] = GridType.Player;
+
+                    // next
+                    if (Board.d[nxt_y, nxt_x] == GridType.Ground)
+                    {
+                        Board.d[nxt_y, nxt_x] = GridType.Box;
+                    }
+                    else if (Board.d[nxt_y, nxt_x] == GridType.Target)
+                    {
+                        Board.d[nxt_y, nxt_x] = GridType.RedBox;
+                    }
+
+
+                }
+                else if (Board.d[nxt_y, nxt_x] == GridType.Brick
+                    || Board.d[nxt_y, nxt_x] == GridType.Box
+                    || Board.d[nxt_y, nxt_x] == GridType.RedBox)
+                {
+                    return false;
+                }
+                else
+                {
+                    throw new Exception("Map Data damaged.");
+                }
+            }
+            // push red box
+            else if (Board.d[bg_y, bg_x] == GridType.RedBox)
+            {
+                if (!hasNextGrid)
+                {
+                    return false;
+                }
+                if (Board.d[nxt_y, nxt_x] == GridType.Ground
+                    || Board.d[nxt_y, nxt_x] == GridType.Target)
+                {
+
+                    // self
+                    if (Board.d[Board.p.y, Board.p.x] == GridType.TarPlayer)
+                    {
+                        Board.d[Board.p.y, Board.p.x] = GridType.Target;
+                    }
+                    else
+                    {
+                        Board.d[Board.p.y, Board.p.x] = GridType.Ground;
+                    }
+
+                    // bg
+                    Board.d[bg_y, bg_x] = GridType.TarPlayer;
+
+                    // next
+                    if (Board.d[nxt_y, nxt_x] == GridType.Ground)
+                    {
+                        Board.d[nxt_y, nxt_x] = GridType.Box;
+                    }
+                    else if (Board.d[nxt_y, nxt_x] == GridType.Target)
+                    {
+                        Board.d[nxt_y, nxt_x] = GridType.RedBox;
+                    }
+
+                }
+                else if (Board.d[nxt_y, nxt_x] == GridType.Brick
+                    || Board.d[nxt_y, nxt_x] == GridType.Box
+                    || Board.d[nxt_y, nxt_x] == GridType.RedBox)
+                {
+                    return false;
+                }
+                else
+                {
+                    throw new Exception("Map Data damaged.");
+                }
+            }
+            // wrong GameState
+            else
+            {
+                throw new Exception("Map Data damaged.");
+            }
+
+            /**
+             * Move Board.p.
+             */
+            switch (o)
+            {
+                case PlayerOperation.MoveUp:
+                {
+                    Board.p.y -= 1;
+                    break;
+                }
+                case PlayerOperation.MoveDown:
+                {
+                    Board.p.y += 1;
+                    break;
+                }
+                case PlayerOperation.MoveLeft:
+                {
+                    Board.p.x -= 1;
+                    break;
+                }
+                case PlayerOperation.MoveRight:
+                {
+                    Board.p.x += 1;
+                    break;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- src/MainPage.xaml.cs
+++ src/MainPage.xaml.cs
@@ -14,72 +14,31 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media.Imaging;
 
 
-
 namespace MyPushBox
 {
-
-    enum GridType
-    {
-        Ground = 1,
-        Target = 2,
-        Box = 3,
-        RedBox = 4, 
-        Player = 5,
-        TarPlayer = 6,
-        Brick = 7,
-        OutSide = 8,
-
-        // RedBox 只能和 Target 互相转化！！！
-    }
-
-    public enum PlayerOperation {
-        MoveUp,
-        MoveRight,
-        MoveDown,
-        MoveLeft,
-    }
-
-    struct BoardInfo
-    {
-        public int rowNum { get; }
-        public int columnNum { get; }
-        public GridType[,] d;
-
-        public BoardInfo(int r, int c)
-        {
-            rowNum = r;
-            columnNum = c;
-            d = new GridType[r, c];
-        }
-    }
-
-    struct Player {
-        public int x;
-        public int y;
-
-        public Player(int _x, int _y) {
-            x = _x;
-            y = _y;
-        }
-    }
-
 
     public sealed partial class MainPage : Page
     {
         private static Dictionary<char, GridType> BlockCode;
         private static Dictionary<GridType, BitmapImage> BlockType;
         private const int GRID_SIZE = 50;
-        private BoardInfo Board;
         private Image[,] Images;
-        private Player player;
-
+        public GameEngine GE;
 
         public MainPage()
         {
+            // layout component initialize
             this.InitializeComponent();
+
+            // UI hashmaps initialize
             this.InitializeProperties();
-            Board = GetBoardInfo().Result;
-            this.InitializeLayout();
+
+            // game engine initialize
+            BoardInfo board = GetBoardInfo().Result;
+            GE = new GameEngine(board);
+
+            // grid initialize
+            this.InitializeGrid();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -93,7 +52,8 @@ namespace MyPushBox
         }
 
 
-        private void InitializeProperties() {
+        private void InitializeProperties() 
+        {
 
             BlockCode = new Dictionary<char, GridType>();
             BlockCode.Add('-', GridType.Ground);  
@@ -112,7 +72,6 @@ namespace MyPushBox
             BitmapImage bit_red_box = new BitmapImage(new Uri("ms-appx:///Assets/Block/RedBox.jpg"));
             BitmapImage bit_outside = new BitmapImage(new Uri("ms-appx:///Assets/Block/Outside.jpg"));
 
-
             BlockType = new Dictionary<GridType, BitmapImage>();
             BlockType.Add(GridType.Ground, bit_ground);
             BlockType.Add(GridType.Brick, bit_brick);
@@ -122,16 +81,13 @@ namespace MyPushBox
             BlockType.Add(GridType.Target, bit_target);
             BlockType.Add(GridType.RedBox, bit_red_box);
             BlockType.Add(GridType.OutSide, bit_outside);
-
         }
-
 
         private async Task<BoardInfo> GetBoardInfo()
         {
             BoardInfo info;
             int player_x = -1;
             int player_y = -1;
-
             try
             {
                 var uri = new Uri("ms-appx:///Assets/Stages/1.txt");
@@ -152,7 +108,8 @@ namespace MyPushBox
                         string[] numStrings = line.Split(' ');
                         int rowCount = Convert.ToInt32(numStrings[0], 10);
                         int columnCount = Convert.ToInt32(numStrings[1], 10);
-                        info = new BoardInfo(rowCount, columnCount);
+                        Player player = new Player(0, 0);
+                        info = new BoardInfo(rowCount, columnCount, player);
 
                         for (int i = 0; i < rowCount; i++)
                         {
@@ -184,7 +141,9 @@ namespace MyPushBox
                             throw new FileLoadException("No Player Position in Stage File.");
                         }
 
-                        player = new Player(player_x, player_y);
+                        info.p.x = player_x;
+                        info.p.y = player_y;
+                       
                     }
                 }
 
@@ -194,15 +153,14 @@ namespace MyPushBox
             {
                 Debug.WriteLine("The file could not be read:");
                 Debug.WriteLine(e.Message);
-                return new BoardInfo(0, 0);
+                return new BoardInfo(0, 0, new Player(0, 0));
             }
         }
 
-
-        private void InitializeLayout()
+        private void InitializeGrid()
         {
-            int rowCount = Board.rowNum;
-            int colCount = Board.columnNum;
+            int rowCount = GE.rowNum;
+            int colCount = GE.columnNum;
             Grid g = grid_table;
             Images = new Image[rowCount, colCount];
 
@@ -221,13 +179,14 @@ namespace MyPushBox
                 g.ColumnDefinitions.Add(rd);
             }
 
-            this.RefreshLayout();
+            this.RefreshGrid();
         }
 
-        private void RefreshLayout() {
+        private void RefreshGrid() {
 
-            int rowCount = Board.rowNum;
-            int colCount = Board.columnNum;
+            int rowCount = GE.rowNum;
+            int colCount = GE.columnNum;
+            GridType[,] d = GE.d;
 
             for (int r = 0; r < rowCount; r++)
             {
@@ -235,277 +194,10 @@ namespace MyPushBox
                 {
                     Image img = new Image();
                     Images[r, c] = img;
-                    img.Source = BlockType[Board.d[r, c]];
+                    img.Source = BlockType[d[r, c]];
                     grid_table.Children.Add(img);
                     Grid.SetColumn(img, c);
                     Grid.SetRow(img, r);
-                }
-            }
-        }
-
-
-        private void PlayerOperate(PlayerOperation o) {
-
-            /**
-             * Get variables.
-             */
-            Image playerImage = Images[player.y, player.x];
-            Image backgroundImage = null;
-            Image nextImage = null;
-
-            int bg_y = -1;
-            int bg_x = -1;
-            int nxt_y = -1;
-            int nxt_x = -1;
-            
-            ref GridType playerGrid = ref Board.d[player.y, player.x];
-
-            switch (o) {
-                case PlayerOperation.MoveUp:
-                {
-                    if (player.y == 0)
-                    {
-                        return;
-                    }
-                    backgroundImage = Images[player.y - 1, player.x];
-                    nextImage = (player.y == 1) ? null : Images[player.y - 2, player.x];
-                    bg_y = player.y - 1;
-                    bg_x = player.x;
-                    nxt_y = player.y - 2;
-                    nxt_x = player.x;
-
-                    break;
-                }
-                case PlayerOperation.MoveDown:
-                {
-                    if (player.y == Board.rowNum - 1)
-                    {
-                        return;
-                    }
-                    backgroundImage = Images[player.y + 1, player.x];
-                    nextImage = (player.y == Board.rowNum - 2) ? null : Images[player.y + 2, player.x];
-
-                    bg_y = player.y + 1;
-                    bg_x = player.x;
-                    nxt_y = player.y + 2;
-                    nxt_x = player.x;
-
-                    break;
-                }
-                case PlayerOperation.MoveLeft:
-                {
-                    if (player.x == 0)
-                    {
-                        return;
-                    }
-                    backgroundImage = Images[player.y, player.x - 1];
-                    nextImage = (player.x == 1) ? null : Images[player.y, player.x - 2];
-
-                    bg_y = player.y;
-                    bg_x = player.x - 1;
-                    nxt_y = player.y;
-                    nxt_x = player.x - 2;
-
-                    break;
-                }
-                case PlayerOperation.MoveRight:
-                {
-                    if (player.x == Board.columnNum - 1)
-                    {
-                        return;
-                    }
-                    backgroundImage = Images[player.y, player.x + 1];
-                    nextImage = (player.x == Board.columnNum - 2) ? null : Images[player.y, player.x + 2];
-
-                    bg_y = player.y;
-                    bg_x = player.x + 1;
-                    nxt_y = player.y;
-                    nxt_x = player.x + 2;
-
-                    break;
-                }
-            }
-
-            /**
-             * Logic Judgement.
-             */
-
-            // reached brick
-            if (backgroundImage.Source == BlockType[GridType.Brick])
-            {
-                return;
-            }
-            // move to ground
-            else if (backgroundImage.Source == BlockType[GridType.Ground])
-            {
-                backgroundImage.Source = BlockType[GridType.Player];
-                Board.d[bg_y, bg_x] = GridType.Player;
-
-                if (Board.d[player.y, player.x] == GridType.TarPlayer)
-                {
-                    playerImage.Source = BlockType[GridType.Target];
-                    Board.d[player.y, player.x] = GridType.Target;
-                }
-                else 
-                {
-                    playerImage.Source = BlockType[GridType.Ground];
-                    Board.d[player.y, player.x] = GridType.Ground;
-                }
-
-            }
-            // move to target
-            else if (backgroundImage.Source == BlockType[GridType.Target])
-            {
-                backgroundImage.Source = BlockType[GridType.TarPlayer];
-                Board.d[bg_y, bg_x] = GridType.TarPlayer;
-
-                if (Board.d[player.y, player.x] == GridType.TarPlayer)
-                {
-                    playerImage.Source = BlockType[GridType.Target];
-                    Board.d[player.y, player.x] = GridType.Target;
-                }
-                else
-                {
-                    playerImage.Source = BlockType[GridType.Ground];
-                    Board.d[player.y, player.x] = GridType.Ground;
-                }
-
-
-            }
-            // push box
-            else if (backgroundImage.Source == BlockType[GridType.Box])
-            {
-                if (nextImage == null)
-                {
-                    return;
-                }
-
-                if (nextImage.Source == BlockType[GridType.Ground]
-                    || nextImage.Source == BlockType[GridType.Target])
-                {
-                    // self
-                    if (Board.d[player.y, player.x] == GridType.TarPlayer)
-                    {
-                        playerImage.Source = BlockType[GridType.Target];
-                        Board.d[player.y, player.x] = GridType.Target;
-                    }
-                    else
-                    {
-                        playerImage.Source = BlockType[GridType.Ground];
-                        Board.d[player.y, player.x] = GridType.Ground;
-                    }
-
-                    // bg
-                    backgroundImage.Source = BlockType[GridType.Player];
-                    Board.d[bg_y, bg_x] = GridType.Player;
-
-                    // next
-                    if (nextImage.Source == BlockType[GridType.Ground])
-                    {
-                        nextImage.Source = BlockType[GridType.Box];
-                        Board.d[nxt_x, nxt_y] = GridType.Box;
-                    }
-                    else if (nextImage.Source == BlockType[GridType.Target])
-                    {
-                        nextImage.Source = BlockType[GridType.RedBox];
-                        Board.d[nxt_x, nxt_y] = GridType.RedBox;
-                    }
-
-
-                }
-                else if (nextImage.Source == BlockType[GridType.Brick]
-                    || nextImage.Source == BlockType[GridType.Box]
-                    || nextImage.Source == BlockType[GridType.RedBox]) 
-                {
-                    return;
-                }
-                else
-                {
-                    throw new Exception("Map Data damaged.");
-                }
-            }
-            // push red box
-            else if (backgroundImage.Source == BlockType[GridType.RedBox])
-            {
-                if (nextImage == null)
-                {
-                    return;
-                }
-
-                if (nextImage.Source == BlockType[GridType.Ground]
-                    || nextImage.Source == BlockType[GridType.Target])
-                {
-
-                    // self
-                    if (Board.d[player.y, player.x] == GridType.TarPlayer)
-                    {
-                        playerImage.Source = BlockType[GridType.Target];
-                        Board.d[player.y, player.x] = GridType.Target;
-                    }
-                    else
-                    {
-                        playerImage.Source = BlockType[GridType.Ground];
-                        Board.d[player.y, player.x] = GridType.Ground;
-                    }
-
-                    // bg
-                    backgroundImage.Source = BlockType[GridType.TarPlayer];
-                    Board.d[bg_y, bg_x] = GridType.TarPlayer;
-
-                    // next
-                    if (nextImage.Source == BlockType[GridType.Ground])
-                    {
-                        nextImage.Source = BlockType[GridType.Box];
-                        Board.d[nxt_x, nxt_y] = GridType.Box;
-                    }
-                    else if (nextImage.Source == BlockType[GridType.Target])
-                    {
-                        nextImage.Source = BlockType[GridType.RedBox];
-                        Board.d[nxt_x, nxt_y] = GridType.RedBox;
-                    }
-
-                }
-                else if (nextImage.Source == BlockType[GridType.Brick]
-                    || nextImage.Source == BlockType[GridType.Box]
-                    || nextImage.Source == BlockType[GridType.RedBox])
-                {
-                    return;
-                }
-                else
-                {
-                    throw new Exception("Map Data damaged.");
-                }
-            }
-            // wrong GameState
-            else
-            {
-                throw new Exception("Map Data damaged.");
-            }
-
-            /**
-             * Move Player.
-             */
-            switch (o)
-            {
-                case PlayerOperation.MoveUp:
-                {
-                    player.y -= 1;
-                    break;
-                }
-                case PlayerOperation.MoveDown:
-                {
-                    player.y += 1;
-                    break;
-                }
-                case PlayerOperation.MoveLeft:
-                {
-                    player.x -= 1;
-                    break;
-                }
-                case PlayerOperation.MoveRight:
-                {
-                    player.x += 1;
-                    break;
                 }
             }
         }
@@ -517,35 +209,37 @@ namespace MyPushBox
             {
                 case VirtualKey.W:
                 case VirtualKey.Up:
-                    {
-                        PlayerOperate(PlayerOperation.MoveUp);
-                        break;
-                    }
+                {
+                    GE.PlayerOperate(PlayerOperation.MoveUp);
+                    RefreshGrid();
+                    break;
+                }
                 case VirtualKey.S:
                 case VirtualKey.Down:
-                    {
-                        PlayerOperate(PlayerOperation.MoveDown);
-                        break;
-                    }
+                {
+                    GE.PlayerOperate(PlayerOperation.MoveDown);
+                    RefreshGrid();
+                    break;
+                }
                 case VirtualKey.D:
                 case VirtualKey.Right:
-                    {
-                        PlayerOperate(PlayerOperation.MoveRight);
-                        break;
-                    }
+                {
+                    GE.PlayerOperate(PlayerOperation.MoveRight);
+                    RefreshGrid();
+                    break;
+                }
                 case VirtualKey.A:
                 case VirtualKey.Left:
-                    {
-                        PlayerOperate(PlayerOperation.MoveLeft);
-                        break;
-                    }
+                {
+                    GE.PlayerOperate(PlayerOperation.MoveLeft);
+                    RefreshGrid();
+                    break;
+                }
                 default:
-                    {
-                        break;
-                    }
+                {
+                    break;
+                }
             }
-            
         }
-
     }
 }
